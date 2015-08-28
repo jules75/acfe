98aaaceb3bbde40450df1fa6372b315d46e8fe0c
Julian Laffey <julianlaffey@gmail.com>
2015-08-28 15:02:24 +1000

    Chart for priority areas - tightly coupled

--- src/acfe/core.clj
+++ src/acfe/core.clj
@@ -113,19 +113,37 @@
 (defn area-html
   "Return HTML representation of given area facts."
   [area-facts]
-  (->
-   (e/html-resource "html/area.html")
-   (e/at
-	[:h2 :span] (e/content (:area (first area-facts)))
-	[:table :thead :td]
-	(e/clone-for
-	 [fact (cons nil area-facts)]
-	 [:td] (e/content (:title fact)))
-	[:table :tbody :tr.area :td]
-	(e/clone-for
-	 [fact (cons {:detail_text "This LGA"} area-facts)]
-	 [:td] (e/content (formatted-fact-detail fact))
-	 ))))
+  (let [grouped-facts (group-by :category area-facts)
+		population-facts (get grouped-facts "Population 2014")
+		priority-facts (get grouped-facts "Priority groups")]
+	(->
+	 (e/html-resource "html/area.html")
+
+	 (e/at
+	  [:h2 :span] (e/content (:area (first area-facts)))
+	  [:table#population :thead :td]
+	  (e/clone-for
+	   [fact (cons nil population-facts)]
+	   [:td] (e/content (:title fact)))
+	  [:table#population :tbody :tr.area :td]
+	  (e/clone-for
+	   [fact (cons {:detail_text "This LGA"} population-facts)]
+	   [:td] (e/content (formatted-fact-detail fact))
+	   ))
+
+	 (e/at
+	  [:h2 :span] (e/content (:area (first area-facts)))
+	  [:table#priority :thead :td]
+	  (e/clone-for
+	   [fact (cons nil priority-facts)]
+	   [:td] (e/content (:title fact)))
+	  [:table#priority :tbody :tr.area :td]
+	  (e/clone-for
+	   [fact (cons {:detail_text "This LGA"} priority-facts)]
+	   [:td] (e/content (formatted-fact-detail fact))
+	   ))
+
+	 )))
 
 
 (defroutes routes
