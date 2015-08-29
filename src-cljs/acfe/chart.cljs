415c7e3e5de37e1c84a91da4890fe68b3d05259b
Julian Laffey <julianlaffey@gmail.com>
2015-08-29 15:54:55 +1000

    Moved legend to above chart, spaced charts apart

--- src-cljs/acfe/chart.cljs
+++ src-cljs/acfe/chart.cljs
@@ -21,13 +21,14 @@
 (defn create-chart
   "Given marked up HTMLTableElement table-element, render Google Chart to target-element.
   chart-type is :bar-chart or :column-chart."
-  [table-element target-element chart-type stacked?]
+  [target-element table-element chart-type stacked?]
   (let [raw-rows (into [] (map htmlrow->cells (-> table-element .-rows)))
 		first-row (conj (first raw-rows) {:role "annotation"})
 		rest-rows (map #(conj % nil) (rest raw-rows))
 		final-rows (map (partial map string->float) (cons first-row rest-rows))
 		data (.arrayToDataTable google.visualization (clj->js final-rows))
 		opts {:title (first first-row)
+			  :legend {:position "top" :maxLines 3}
 			  :isStacked (if stacked? "percent" false)}
 		chart (case chart-type
 				:bar-chart (new google.visualization.BarChart target-element)
@@ -41,12 +42,20 @@
   []
 
   (doseq [table (d/sel [:.chart.bar.stacked])]
-	(create-chart table (d/insert-before! (d/create-element :div) table) :bar-chart true)
+	(->
+	 (d/create-element :div)
+	 (d/add-class! "chart")
+	 (d/insert-before! table)
+	 (create-chart table :bar-chart true))
 	(d/remove-class! table "chart") ; so table isn't converted to chart again
 	(d/hide! table))
 
   (doseq [table (d/sel [:.chart.column])]
-	(create-chart table (d/insert-before! (d/create-element :div) table) :column-chart false)
+	(->
+	 (d/create-element :div)
+	 (d/add-class! "chart")
+	 (d/insert-before! table)
+	 (create-chart table :column-chart false))
 	(d/remove-class! table "chart")
 	(d/hide! table))
 
